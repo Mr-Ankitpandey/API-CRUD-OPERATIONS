604ff55c17428cbdf6722ebebea2ddc2709c31fc
SoftDev728 <testdev728@gmail.com>
2026-03-24 16:05:15 +0530

    new function

---
+++ src/practice.tsx
@@ -0,0 +1,15 @@
+
+const practice = () => {
+
+    const logUser = ()=> {
+        console.log("logging user")
+    }
+    logUser()
+  return (
+    <div>
+      <h1>Git branching</h1>
+    </div>
+  )
+}
+
+export default practice
